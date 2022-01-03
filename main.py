ed54b4ee767359647e2fb5c8c4339053e4a1396a
TeamSlapTap <96988572+TeamSlapTap@users.noreply.github.com>
2022-01-03 17:02:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ bot = telebot.TeleBot("5088301449:AAEYbFdLo1iZwg7tfNDxHM1cbrNojA-drCE")
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-  bot.reply_to(message, "Hello! I'm Garry.[supunma <sz/>](https://t.me/szteambots)")
-BUTTONS = ( [ [ InlineKeyboardButton( text="🆘 Help 🆘", callback_data = "helpmenu_" ), InlineKeyboardButton( text="📊 Stats 📊", callback_data="stats_callback", ), ], [ InlineKeyboardButton( text="🗣 Updates", url="https://t.me/szteambots" ), InlineKeyboardButton( text="👥 Support", url="https://t.me/slbotzone", ), ], [ InlineKeyboardButton( text="➕ Add Me To Your Group ➕", url=f"t.me/szrosebot?startgroup=true", ) ], ])
-
+  bot.reply_to(message, "Hello! I'm Garry.")
 
 @bot.message_handler(commands=["how"])
 def send_message(message):
