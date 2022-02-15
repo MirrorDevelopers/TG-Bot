2cbad5245db67dda45d4e348aefd57d91c3f29e3
DarkZ <93233687+MirrorDevelopers@users.noreply.github.com>
2022-02-15 08:20:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ def send_welcome(message):
 
 @bot.message_handler(commands=["how"])
 def send_message(message):
-  bot.send_message(message.chat.id, "https://t.me/AnyStudent")
+  bot.send_message(message.chat.id, "Oh Wanna Be A Team Member.Send Your Name,Username,Verification (required),Performance Of :- eg:Pyrogram, Making Par")
 
+@bot.message_handler(commands=["offers"])
+def send_message(message):
+  bot.send_message(message.chat.id, "Aztre Free Shipping For Asian Countries Use Promo Code TheShadowPro 50% Off Valid Till February 16 2024")
 
 
 bot.polling()
